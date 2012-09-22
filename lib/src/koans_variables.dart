3e7e6b2c59816d13b40be580575a1ddd273cad8d
Matthew Butler <butler.matthew@gmail.com>
2012-09-22 17:29:31 -0300

    Completed introduction.

--- lib/src/koans_variables.dart
+++ lib/src/koans_variables.dart
@@ -1,23 +1,30 @@
 start_here() {
-  group('Variable Types -', () {
-    group('Boolean:', () {
-      test('true', () {
-        /*
-         * This is your first test. Throughout these files you will be
-         * tasked with correcting each test to proceed to the next.
-         * It is recommended you only correct one component at a time
-         * and re-run the application to verify your fix.
-         * Below, change _____ to be true and try to proceed.
-         */
-        expect(_____, true);
-        
+  group('Introduction -', () {
+    test('First steps', () {
+      /*
+       * This is your first test. Throughout these files you will be
+       * tasked with correcting each test to proceed to the next.
+       * It is recommended you only correct one component at a time
+       * and re-run the application to verify your fix.
+       * Below, change _____ to be false and try to proceed.
+       */
+        expect(_____, false);
       });
     
-      test('false', () {
-        
-        expect(_____, false);
-        
-      });
+    test('Beginning', () {
+      /*
+       * The comparison on the end will not always be a specific
+       * type, however between the context, comments, and most
+       * importantly, the error message you receive when you try
+       * to run the program, should tell you what you need to do
+       * to make the test pass.
+       */
+      expect(_____, isTrue);
+    });
+  });
+  group('Variables -', () {
+    group('Optional Types', () {
+      
     });
   });
 }
