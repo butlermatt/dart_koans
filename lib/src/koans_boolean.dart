95dd37119b68af040627660d3d1485b25284c608
Matthew Butler <butler.matthew@gmail.com>
2012-09-23 19:32:43 -0300

    Started on booleans.

---
+++ lib/src/koans_boolean.dart
@@ -0,0 +1,12 @@
+void using_boolean() {
+  group('Boolean -', () {
+    group('Declaring -', () {
+      test('bool', () {
+        /*
+         * Perhaps the similest type in Dart is the boolean (bool)
+         * Booleans accept only true or false values.
+         */
+      });
+    });
+  });
+}
