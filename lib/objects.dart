dff76f4b304123704c583d4fe931107f5b74c690
Matthew Butler <butler.matthew@gmail.com>
2012-09-24 13:07:08 -0300

    Add objects methods section

--- lib/objects.dart
+++ lib/objects.dart
@@ -21,11 +21,36 @@ void using_objects() {
         /*
          * Even literals are objects in Dart. Check if
          * 5 isNum and if it isObject.
+         * 
          * Note that the isXxxx notation is the the correct
          * way of checking a type. We'll look at that later
          * as well.
          */
         expect(5, _____);
+      });
+    });
+    group('Methods -', () {
+      test('toString()', () {
+        /*
+         * Because everything is an object, it means that we
+         * can call their methods to interact with them.
+         * Even operators such as ==, =, +, -, etc are methods
+         * though we can't call them explicity when we do
+         * something like 5 + 3 it's the same as 5.+(3);
+         */
+        expect(5.32.toString(), equals('_____'));
+      });
+      test('Type specific', () {
+        /* 
+         * In addition to the shared methods of Object, each
+         * 'type' (or Class as we will see later) has its own
+         * selection of methods as well. Such as a check to
+         * see if a number is positive or negative.
+         * Hint: isNegative() method returns a boolean
+         * (true/false) value).
+         */
+        var number = -3;
+        expect(number.isNegative(), equals(_____));
       });
     });
   });
