798a695249714b44d233d12035f29de94b356772
Matthew Butler <butler.matthew@gmail.com>
2012-09-23 22:11:24 -0300

    Renamed koans files. Less typing = good.

--- bin/dart_koans.dart
+++ bin/dart_koans.dart
@@ -4,9 +4,9 @@
 #import('package:dart_koans/src/config_koans.dart');
 #import('package:dart_koans/src/config_colors.dart', prefix: 'colors');
 
-#source('package:dart_koans/koans_numbers.dart');
-#source('package:dart_koans/koans_boolean.dart');
-#source('package:dart_koans/koans_strings.dart');
+#source('package:dart_koans/numbers.dart');
+#source('package:dart_koans/boolean.dart');
+#source('package:dart_koans/strings.dart');
 
 const _____ = '<Fill in Value>';
 
