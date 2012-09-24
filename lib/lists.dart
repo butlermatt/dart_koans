86852630916655ceab7f3c1183e7a5fe6ce53d7b
Matthew Butler <butler.matthew@gmail.com>
2012-09-24 15:57:33 -0300

    Started adding lists.

---
+++ lib/lists.dart
@@ -0,0 +1,17 @@
+void using_lists() {
+  group('Lists -', () {
+    group('Declaring -' () {
+      test('literal', () {
+        /*
+         * A List in Dart is similar to an array in other
+         * languages. Specifically it is an indexable
+         * collection of values. A List literal is defined
+         * between two square braces [ and ].
+         * Fill in the literal that list should equal below.
+         */
+        List list = [3, 2, 5];
+        expect(list, equals(_____))
+      });
+    });
+  });
+}
