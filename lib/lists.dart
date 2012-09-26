327f083829d142860cbadcfc44ed4bca549d41a4
Matthew Butler <butler.matthew@gmail.com>
2012-09-25 21:56:02 -0300

    Added List construct and Generics.

--- lib/lists.dart
+++ lib/lists.dart
@@ -20,6 +20,28 @@ void using_lists() {
         var list = [3, 2, 4.5, 0.03, 'string', ['sublist', true], false];
         expect(_____, isList);
       });
+      test('Constructor', () {
+        /*
+         * Lists can also be created with a constructor. We'll 
+         * learn about constructors more later on but lets take
+         * a peak now as its fairly common way of declaring a
+         * list.
+         */
+        var list = new List();
+        expect(_____, isList);
+      });
+      test('Using Generics', () {
+        /*
+         * Occasionally, you may which to specify that a list
+         * should only contain certain types. While dart is an
+         * optionally typed language, and ultimately you can
+         * assign any value to any variable (type), you can run
+         * dart in 'checked-mode' which will give you warnings
+         * or errors when unexpected types appear.
+         */
+        var list = new List<int>();
+        expect(_____, isList);
+      });
     });
   });
 }
