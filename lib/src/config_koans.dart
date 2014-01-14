2d62e4efd50e3235bc336396bb3b76be1f18c422
politician <david.zarlengo@gmail.com>
2014-01-13 18:03:27 -0800

    upgrade dependencies, and fix affected files

--- lib/src/config_koans.dart
+++ lib/src/config_koans.dart
@@ -109,12 +109,14 @@ class ConfigKoans extends ut.SimpleConfiguration {
       components = components.split(' ');
       var fileName = components[0];
       components = components[1].split(':');
-      var pathBuild = new path.Builder(root: LIB_DIR);
+
+      var context = new path.Context(style: path.Style.windows);
+      var lookInFile = context.join(LIB_DIR, fileName);
       
       print('Failed at: ${colors.DK_YELLOW(fail.description)}');
       print(fail.message);
       print('Seek your answers in File:');
-      print('${colors.DK_MAGENTA(pathBuild.resolve(fileName)) } ' 
+      print('${colors.DK_MAGENTA(lookInFile) } ' 
         '(Line: ${components[0]} Column: ${components[1]})\n');
     } 
     
